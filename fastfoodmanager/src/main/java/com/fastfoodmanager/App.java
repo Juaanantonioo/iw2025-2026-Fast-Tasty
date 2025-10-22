e5602de90af2c7ed166d6b9b2616d62ccc6ab94b
juanantonio <martinsalvado22@gmail.com>
2025-10-22 12:23:06 +0200

    Creación de la estructura de carpetas con maven

---
+++ fastfoodmanager/src/main/java/com/fastfoodmanager/App.java
@@ -0,0 +1,13 @@
+package com.fastfoodmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
